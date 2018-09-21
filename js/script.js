9d04b85f2aa96a777d71e00861d8ae4a4ff31511
PavelVolk <42099520+PavelVolk@users.noreply.github.com>
2018-09-21 22:33:20 +0300

    Внёс однообразие

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ form.addEventListener("submit", function(evt) {
   }
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("search-form-show")) {
