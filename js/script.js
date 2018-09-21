1414ffcffd75b067639395a1deb6c6e677518372
PavelVolk <42099520+PavelVolk@users.noreply.github.com>
2018-09-21 20:42:01 +0300

    Подключил минимизированный css и js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
       var link = document.querySelector(".search-link");
 
-      var popup = document.querySelector(".search-form");
+      var popup = document.querySelector(".search-wrapper");
       var form = document.querySelector(".search-form")
 
       var arrival = document.querySelector("#arrival")
@@ -9,7 +9,12 @@
 
       link.addEventListener("click", function (evt) {
         evt.preventDefault();
-        popup.classList.toggle("search-form-show");
+        form.classList.toggle("search-form-show");
+        popup.classList.toggle("search-wrapper-show")
+        if (popup.classList.contains("search-form-error")) {
+          popup.classList.remove("search-form-error")
+        }
+
       });
 
       form.addEventListener("submit", function(evt) {
@@ -25,7 +30,8 @@
         if (evt.keyCode === 27) {
           evt.preventDefault();
           if (popup.classList.contains("search-form-show")) {
-            popup.classList.remove("search-form-show");
+            popup.classList.remove("search-wrapper-show");
+            form.classList.remove("search-form-show");
           }
         }
       });
