897e99b621be8dcea9d7ce3d5a5378111173e885
PavelVolk <42099520+PavelVolk@users.noreply.github.com>
2018-09-21 14:44:30 +0300

    Изменил попап

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+      var link = document.querySelector(".search-link");
+
+      var popup = document.querySelector(".search-form");
+      var form = document.querySelector(".search-form")
+
+      var arrival = document.querySelector("#arrival")
+      var leaving = document.querySelector("#leaving")
+
+      link.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.toggle("search-form-show");
+      });
+
+      form.addEventListener("submit", function(evt) {
+        if (!arrival.value || !leaving.value) {
+          evt.preventDefault();
+          popup.classList.remove("search-form-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("search-form-error");
+        }
+      });
+
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (popup.classList.contains("search-form-show")) {
+            popup.classList.remove("search-form-show");
+          }
+        }
+      });
